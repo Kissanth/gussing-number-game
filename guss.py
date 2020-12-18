feda6c096091fefbb2e8f71c4831efd140cf2a44
Kissanth <55798242+Kissanth@users.noreply.github.com>
2020-12-18 19:28:46 +0530

    Add files via upload

---
+++ guss.py
@@ -0,0 +1,9 @@
+print("Hi Would you like to Play with python ")
+print("let get start ")
+print("first thing you should guss the number  then enter the multiplyed number :")
+n=int(input("Then you can Multiply the number with 2  then enter the multiplyed number:"))
+l=int(input("then You can also Multiply with 5 with your already multiply number then enter the multiplyed number: "))
+o=int(input("You can also Two more thing one You can multiply with 2 then enter the multiplyed number: "))
+p=int(input("then You can also Multiply with 5 with your already multiply number  then enter the multiplyed number: "))
+t=p/100
+print("Your gussed Number is " , t)
